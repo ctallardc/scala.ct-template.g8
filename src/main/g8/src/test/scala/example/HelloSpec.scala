9fa2bf7bc8f9c20e69e9fa162d979c165d9f0852
Carlos Tallard <ctallardc@yahoo.com>
2025-04-28 10:09:21 -0400

    updated .gitignore and new test file

--- src/main/g8/src/test/scala/example/HelloSpec.scala
+++ src/main/g8/src/test/scala/example/HelloSpec.scala
@@ -4,4 +4,35 @@ class HelloSpec extends munit.FunSuite {
   test("say hello") {
     assertEquals(Hello.greeting, "hello")
   }
+  test("one plus one is two")(assert(1 + 1 == 2))
+
+  test("intNotZero throws an exception if its argument is 0") {
+    intercept[IllegalArgumentException] {
+      intNotZero(0)
+    }
+  }
+  def intNotZero(x: Int): Int = {
+    if (x == 0) throw new IllegalArgumentException("zero is not allowed")
+    else x
+  }
+
+
+import myutils._
+
+test("sum of a few numbers") {
+    assert(Lists.sum(List(1,2,0)) == 3)
+  }
+  
+  test("max of a few numbers") {
+    assert(Lists.sum(List(1,2,0)) == 3)
+    assert(Lists.max(List(3, 7, 2)) == 7)
+  }
+  
+   test("max of Empty") {
+     intercept[NoSuchElementException] {
+    	 Lists.max(List())
+     }
+  }
+  
+
 }
